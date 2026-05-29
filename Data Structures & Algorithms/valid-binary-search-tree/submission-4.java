7490d648f8eb74af435dfa9e65bc8f1ea87dc48a
suman12362 <68195383+suman12362@users.noreply.github.com>
2026-05-30 04:41:06 +0530

    Add: valid-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-4.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+  List<Integer> ls = new ArrayList<>();
+    public boolean isValidBST(TreeNode root) {
+      if(root == null) return false;
+       // List<Integer> b = 
+       verifyBst(root);
+        //System.out.println(ls.size());
+        for(int i = 0;i<ls.size()-1;i++){
+          //System.out.println(ls.get(i)+" sum "+ls.get(i+1));
+          if(ls.get(i) >= ls.get(i+1)){
+          return false;
+          }
+        }
+        return true;
+    }
+
+    public void verifyBst(TreeNode root){
+     // if(root == null || root.left == null || root.right == null) return false;
+     // if((root.left).val > root.val) return false;
+     if(root != null) {
+    //  return null;
+     //List<Integer> ls = new ArrayList<>();
+     //if(root != null){
+      verifyBst(root.left);
+      ls.add(root.val);
+     // System.out.println(root.val);
+      //if((root.right).val < root.val) return false;
+      verifyBst(root.right);}
+     // return true;
+    //}
+    //return ls;
+    }
+}
