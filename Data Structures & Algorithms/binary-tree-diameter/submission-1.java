cda36ed15cfa7c50f15290013d8bb327e55797b2
suman12362 <68195383+suman12362@users.noreply.github.com>
2026-06-05 00:47:59 +0530

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    int d = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        findDia(root);
+        return d;
+    }
+  
+     public int findDia(TreeNode root) {
+        if(root == null) return 0;
+        int l = findDia(root.left);
+        int r = findDia(root.right);
+        d = Math.max(d, l+r);
+           return  1 + Math.max(l,r); 
+          
+        }
+     
+    }
+
+
+// Stack<TreeNode> stk = new Stack<>();
+        // stk.push(root);
+        // while(!stk.isEmpty()){
+            
+        //     TreeNode nd = stk.pop();
+        //     System.out.println(nd.val);
+        //     if(nd.right != null ) stk.push(nd.right);
+        //     if(nd.left != null ) stk.push(nd.left);
+            //System.out.println(nd.val);
+            //if(nd.right != null ) stk.push(nd.right);
+           //System.out.println(nd.val);
+            
+            
+            //if(!stk.isEmpty()){
+            //System.out.println(stk.peek().val);
+            //}
