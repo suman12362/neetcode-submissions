51354f7c1dc25aad611e5a7a7a043c5316f875c8
suman12362 <68195383+suman12362@users.noreply.github.com>
2026-06-07 14:02:50 +0530

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.java
@@ -0,0 +1,88 @@
+class WordDictionary {
+
+static class Node{
+  Node[] child;
+  boolean eow;
+  Node(){
+    child = new Node[26];
+    eow = false;
+    for(int i =0;i<26;i++){
+      child[i] = null;
+    }
+  }
+}
+Node root;
+    public WordDictionary() {
+      root = new Node();
+    }
+
+    public void addWord(String word) {
+       
+       Node cur = root;
+       for(int i =0;i<word.length();i++){
+        int idx = word.charAt(i) -'a';
+        if(cur.child[idx] == null){
+          cur.child[idx] = new Node();
+          //cur = cur.child[idx];
+        }
+        cur = cur.child[idx];
+        if(i == word.length()-1){
+          cur.eow = true;
+        }
+       }
+    }
+
+    public boolean search(String word) {
+       
+       Node cur = root;
+       return searchfordot(root,word,0);
+      //  for(int i =0;i<word.length();i++){
+      //   int idx = word.charAt(i) -'a';
+      //   if(word.charAt(i) != '.'){
+      //   if(cur.child[idx] == null){
+      //     return false;
+      //     //cur = cur.child[idx];
+      //   }
+      //   cur = cur.child[idx];
+      //  }else{
+      //   return searchfordot(root,word,0);
+      //   // for(int i =0;i<26;i++){
+      //   //   if(cur.child[i] !=null){
+      //   //     cur = cur.child[idx];
+      //   //   }
+      //   // }
+      //  }
+      //  }
+      //  return cur.eow;
+    }
+
+    public boolean searchfordot(Node cur,String word,int curchar) {
+      //Node cur = root;
+      if(curchar == word.length()){
+          return cur.eow;
+        }
+     // for(int k = curchar;k<word.length();k++){
+      
+        if(word.charAt(curchar) == '.'){
+       for(int i =0;i<26;i++){
+          if(cur.child[i] !=null){
+            //cur = cur.child[i];
+            if(searchfordot(cur.child[i],word,curchar+1))
+            return true;
+          }
+        }
+        return false;
+      }else{
+        int idx = word.charAt(curchar)- 'a';
+        if(cur.child[idx] != null){
+        return searchfordot(cur.child[idx],word,curchar+1);
+        }
+        else{
+          return false;
+        }
+      }
+       // }
+    //  }
+   //return false;
+    }
+}
